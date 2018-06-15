ec74ecbdde1da63edf2d54e8f7d340aa5249a9d4
韩承海 <374802597@qq.com>
2018-06-15 15:17:42 +0800

    回家学习

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,25 +3,30 @@
 const app = getApp()
 
 Page({
+
+
+  //页面初始数据
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
+  //事件处理函数 页面中定义的方法绑定
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+
+
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,6 +47,30 @@ Page({
         }
       })
     }
+  },
+  onReady() {
+
+  },
+
+  onPullDownRefresh() {
+
+  },
+
+//垂直方向的px
+  onPageScroll(scrollTop) {
+
+  },
+//右上角的转发
+  onShareAppMessage: function () {
+    return {
+      title: '自定义转发标题',
+      path: '/page/user?id=123'
+    }
+  },
+  onTabItemTap(item) {
+    console.log(item.index)
+    console.log(item.pagePath)
+    console.log(item.text)
   },
   getUserInfo: function(e) {
     console.log(e)
