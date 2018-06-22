7d24dd24dbc17d0738352dc664a4b7bd7727d6d7
韩承海 <374802597@qq.com>
2018-06-22 18:27:45 +0800

    no message

--- mpVueApp/src/pages/index/index.vue
+++ mpVueApp/src/pages/index/index.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="container" @click="clickHandle('test click', $event)">
-
     <div class="userinfo" @click="bindViewTap">
       <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
       <div class="userinfo-nickname">
         <card :text="userInfo.nickName"></card>
       </div>
     </div>
-
     <div class="usermotto">
       <div class="user-motto">
         <card :text="motto"></card>
       </div>
     </div>
-
     <form class="form-container">
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
@@ -21,12 +18,11 @@
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
   </div>
 </template>
-
 <script>
 import card from '@/components/card'
 
 export default {
-  data () {
+  data() {
     return {
       motto: 'Hello World',
       userInfo: {}
@@ -38,11 +34,11 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
+    bindViewTap() {
       const url = '../logs/main'
       wx.navigateTo({ url })
     },
-    getUserInfo () {
+    getUserInfo() {
       // 调用登录接口
       wx.login({
         success: () => {
@@ -54,18 +50,18 @@ export default {
         }
       })
     },
-    clickHandle (msg, ev) {
+    clickHandle(msg, ev) {
       console.log('clickHandle:', msg, ev)
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
     this.getUserInfo()
   }
 }
-</script>
 
+</script>
 <style scoped>
 .userinfo {
   display: flex;
@@ -102,4 +98,5 @@ export default {
   color: blue;
   border: 1px solid blue;
 }
+
 </style>
